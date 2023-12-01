3c1a4b20d23e26334d64c3bf13d958662fc593d3
Tanvir <5takarcoin@gmail.com>
2023-12-01 21:40:35 +0600

    Updated Links

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -9,7 +9,8 @@ const Projects = () => {
     {
       id: 1,
       name: "Adda",
-      picture: "https://www.linkpicture.com/q/adacasca.png",
+      picture:
+        "https://scontent.fjsr8-1.fna.fbcdn.net/v/t39.30808-6/405300279_1767565263763821_9081097629108066908_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeGrqZRgUyCGXu4k2Bl0aYoLzphzuQoeTm3OmHO5Ch5ObX1NM2w8Q7weoLw-x6O3a-ibnWk1SBpvU4kSG8oDr122&_nc_ohc=cIACklagkCoAX8U4evC&_nc_ht=scontent.fjsr8-1.fna&oh=00_AfAvYMnqg-NGGPW0yL1QhBlvBxdeE3nBpzaAZTU89DV5Uw&oe=656F5C8B",
       desct: "A realtime chat app",
       descd: [
         `Adda, which is the Bengali word for "chat,"  is a chatting app. Register, login, and find your friends.`,
@@ -23,7 +24,8 @@ const Projects = () => {
     {
       id: 2,
       name: "Blog",
-      picture: "https://www.linkpicture.com/q/asasasasa.png",
+      picture:
+        "https://scontent.fjsr8-1.fna.fbcdn.net/v/t39.30808-6/405381943_1767565253763822_6551002447490281167_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeFyGQ3YjGfgqPH8bVfpuuJ2IPcBmfpFVk4g9wGZ-kVWTrCrUd1By1gjmel9NL3ajEshY-X3Y6lDWsxQzG33tfwE&_nc_ohc=e5C3b8kggo4AX-qy26x&_nc_ht=scontent.fjsr8-1.fna&oh=00_AfApsgqIaEtq8O1rCCkojy2JmTvcRzhi_MfdkHNcgILfyg&oe=656E2561",
       desct: "MERN stack Blog Application",
       descd: [
         `Register, Login and start writing. It can take some time to load, so please be patient.`,
@@ -36,7 +38,8 @@ const Projects = () => {
     {
       id: 3,
       name: "SeeSort",
-      picture: "https://www.linkpicture.com/q/awdafa.png",
+      picture:
+        "https://scontent.fjsr8-1.fna.fbcdn.net/v/t39.30808-6/406905638_1767564777097203_8341692214026824996_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeHLjCqsidU7qEEmFa4QmCtJuJb8rUPuO9O4lvytQ-470-KgOYg7igHFnarYpB3xlngQIVjcOyFIlEppJ2p4hc-U&_nc_ohc=BEv4-M8PBfkAX93OVME&_nc_ht=scontent.fjsr8-1.fna&oh=00_AfBp-tSrq_gGQIe5Dtg8kE3YUrUY9CMbfLxnGVFSG_a1qg&oe=656E4227",
       desct: "Sorting Algorithm Visualizer",
       descd: [`See how Bubble Sort works.`],
       weblink: "https://anv-see-sort.netlify.app/",
